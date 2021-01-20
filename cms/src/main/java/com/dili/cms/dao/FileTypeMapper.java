69168b2c26d32297315fac860ee0c26f2a7bbe45
Tab.Xie <1qaz2wsx>
2021-01-20 17:10:45 +0800

    文件类型Mapper

---
+++ cms/src/main/java/com/dili/cms/dao/FileTypeMapper.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) DiliGroup. All Rights Reserved.
+ * <p>
+ * FileTypeMapper.java created on 2021/1/20 15:33 by Tab.Xie
+ */
+package com.dili.cms.dao;
+
+import com.dili.cms.domain.FileType;
+import com.dili.ss.base.MyMapper;
+
+/**
+ * <pre>
+ * Description
+ * TODO 文件类型Mapper
+ *
+ * @author Tab.Xie
+ * @since 1.0
+ *
+ * Change History
+ * Date  Modifier  DESC.
+ * 2021/1/20  Tab.Xie  Initial version.
+ * </pre>
+ */
+public interface FileTypeMapper extends MyMapper<FileType> {
+}
