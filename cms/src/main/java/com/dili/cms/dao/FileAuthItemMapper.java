3545724a0e6f46f6aaaa031222f822946637f1f1
Tab.Xie <1qaz2wsx>
2021-01-20 17:09:48 +0800

    文件具体权限Mapper

---
+++ cms/src/main/java/com/dili/cms/dao/FileAuthItemMapper.java
@@ -0,0 +1,25 @@
+/**
+* Copyright (C) DiliGroup. All Rights Reserved.
+*
+* FileAuthItemMapper.java created on 2021/1/20 15:32 by Tab.Xie
+*/
+package com.dili.cms.dao;
+
+import com.dili.cms.domain.FileAuthItem;
+import com.dili.ss.base.MyMapper;
+
+/**
+* <pre>
+* Description
+* TODO 文件具体权限Mapper
+*
+* @author Tab.Xie
+* @since 1.0
+*
+* Change History
+* Date  Modifier  DESC.
+* 2021/1/20  Tab.Xie  Initial version.
+* </pre>
+*/
+public interface FileAuthItemMapper extends MyMapper<FileAuthItem> {
+}
