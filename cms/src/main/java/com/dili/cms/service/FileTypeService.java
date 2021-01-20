0d42d604b39e22aa05b2b7ef347cfeb8d9e0abfc
Tab.Xie <1qaz2wsx>
2021-01-20 17:11:20 +0800

    文件类型Service

---
+++ cms/src/main/java/com/dili/cms/service/FileTypeService.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) DiliGroup. All Rights Reserved.
+ * <p>
+ * FileTypeService.java created on 2021/1/20 15:40 by Tab.Xie
+ */
+package com.dili.cms.service;
+
+import com.dili.cms.domain.FileType;
+import com.dili.ss.base.BaseService;
+
+/**
+ * <pre>
+ * Description
+ * TODO 文件类型管理
+ *
+ * @author Tab.Xie
+ * @since 1.0
+ *
+ * Change History
+ * Date  Modifier  DESC.
+ * 2021/1/20  Tab.Xie  Initial version.
+ * </pre>
+ */
+public interface FileTypeService extends BaseService<FileType, Long> {
+}
