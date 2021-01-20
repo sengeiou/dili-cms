375f4918ab9a6126a1a50547f077ea9dc840a5a9
Henry.Huang <henry.huang@skywares.com>
2021-01-20 16:21:38 +0800

    信息通告实体

---
+++ cms-sdk/src/main/java/com/dili/cms/dto/AnnunciateDto.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) DiliGroup. All Rights Reserved.
+ * <p>
+ * AnnunciateDto created on 2021/1/20 14:23 by Henry.Huang
+ */
+package com.dili.cms.dto;
+
+import com.dili.cms.domain.Annunciate;
+import com.dili.ss.metadata.FieldEditor;
+import com.dili.ss.metadata.annotation.EditMode;
+import com.dili.ss.metadata.annotation.FieldDef;
+
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * <pre> 
+ * Description 
+ * 信息通告dto
+ *
+ * @author Henry.Huang
+ * @since 1.0
+ *
+ * Change History 
+ * Date Modifier DESC. 
+ * 2021/1/20 Henry.Huang Initial version. 
+ * </pre> 
+ */
+public interface AnnunciateDto extends Annunciate {
+
+    /**
+     * 通告创建时间范围
+     * @return
+     */
+    LocalDateTime getCreateTimeStart();
+
+    void setCreateTimeStart(LocalDateTime createTimeStart);
+
+    LocalDateTime getCreateTimeEnd();
+
+    void setCreateTimeEnd(LocalDateTime createTimeEnd);
+
+    /**
+     * 发送对象
+     * @return
+     */
+    Integer getTargetType();
+
+    void setTargetType(Integer targetType);
+}
