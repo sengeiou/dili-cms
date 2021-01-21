8261741dadac10f412b942c426689fa21b43a5d0
Henry.Huang <henry.huang@skywares.com>
2021-01-21 14:41:13 +0800

    信息通告

---
+++ cms-sdk/src/main/java/com/dili/cms/sdk/domain/AnnunciateItem.java
@@ -0,0 +1,88 @@
+/**
+  * Copyright (C) DiliGroup. All Rights Reserved. 
+  *
+  * AnnunciateItem.java created on 2021/1/21 14:39 by Henry.Huang 
+  */
+package com.dili.cms.sdk.domain;
+
+import com.dili.ss.dto.IBaseDomain;
+import com.dili.ss.metadata.FieldEditor;
+import com.dili.ss.metadata.annotation.EditMode;
+import com.dili.ss.metadata.annotation.FieldDef;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+  * <pre>
+  * Description
+  * 通告项，通告对应的用户id或客户id的一对一关系
+  *
+  * @author Henry.Huang
+  * @since 1.0
+  *
+  * Change History
+  * Date Modifier DESC.
+  * 2021/1/21 Henry.Huang Initial version.
+  * </pre>
+  */
+@Table(name = "`annunciate_item`")
+public interface AnnunciateItem extends IBaseDomain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "`id`")
+    @FieldDef(label="主键")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Long getId();
+
+    void setId(Long id);
+
+    @Column(name = "`annunciate_id`")
+    @FieldDef(label="通告id")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Long getAnnunciateId();
+
+    void setAnnunciateId(Long annunciateId);
+
+    @Column(name = "`target_id`")
+    @FieldDef(label="目标id")
+    @EditMode(editor = FieldEditor.Number, required = false)
+    Long getTargetId();
+
+    void setTargetId(Long targetId);
+
+    @Column(name = "`read_state`")
+    @FieldDef(label="读取状态")
+    @EditMode(editor = FieldEditor.Text, required = false)
+    Integer getReadState();
+
+    void setReadState(Integer readState);
+
+    @Column(name = "`send_time`")
+    @FieldDef(label="发送时间")
+    @EditMode(editor = FieldEditor.Datetime, required = false)
+    LocalDateTime getSendTime();
+
+    void setSendTime(LocalDateTime sendTime);
+
+    @Column(name = "`read_time`")
+    @FieldDef(label="读消息时间")
+    @EditMode(editor = FieldEditor.Datetime, required = false)
+    LocalDateTime getReadTime();
+
+    void setReadTime(LocalDateTime readTime);
+
+    @Column(name = "`terminal`")
+    @FieldDef(label="阅读终端")
+    @EditMode(editor = FieldEditor.Text, required = false)
+    Integer getTerminal();
+
+    void setTerminal(Integer terminal);
+
+    @Column(name = "`create_time`")
+    @FieldDef(label="创建时间")
+    @EditMode(editor = FieldEditor.Datetime, required = true)
+    LocalDateTime getCreateTime();
+
+    void setCreateTime(LocalDateTime createTime);
+}
---
+++ cms-sdk/src/main/java/com/dili/cms/sdk/domain/AnnunciateTarget.java
@@ -0,0 +1,66 @@
+/**
+  * Copyright (C) DiliGroup. All Rights Reserved. 
+  *
+  * AnnunciateTarget.java created on 2021/1/21 14:40 by Henry.Huang 
+  */
+package com.dili.cms.sdk.domain;
+
+import com.dili.ss.dto.IBaseDomain;
+import com.dili.ss.metadata.FieldEditor;
+import com.dili.ss.metadata.annotation.EditMode;
+import com.dili.ss.metadata.annotation.FieldDef;
+
+import javax.persistence.*;
+
+/**
+  * <pre> 
+  * Description 
+  * 通告目标
+  *
+  * @author Henry.Huang 
+  * @since 1.0 
+  *
+  * Change History 
+  * Date Modifier DESC. 
+  * 2021/1/21 Henry.Huang Initial version. 
+  * </pre> 
+  */
+@Table(name = "`annunciate_target`")
+public interface AnnunciateTarget extends IBaseDomain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "`id`")
+    @FieldDef(label="主键")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Long getId();
+
+    void setId(Long id);
+
+    @Column(name = "`annunciate_id`")
+    @FieldDef(label="通告id", maxLength = 4000)
+    @EditMode(editor = FieldEditor.Text, required = false)
+    String getAnnunciateId();
+
+    void setAnnunciateId(String annunciateId);
+
+    @Column(name = "`target_type`")
+    @FieldDef(label="对象类型")
+    @EditMode(editor = FieldEditor.Text, required = false)
+    Integer getTargetType();
+
+    void setTargetType(Integer targetType);
+
+    @Column(name = "`target_range`")
+    @FieldDef(label="对象范围, 0: 所有用户, 1:指定用户, 2:部门, 3:公司, 4: 司机, 5: 买家, 6: 经营户")
+    @EditMode(editor = FieldEditor.Text, required = false)
+    Integer getTargetRange();
+
+    void setTargetRange(Integer targetRange);
+
+    @Column(name = "`target_range_id`")
+    @FieldDef(label="对象范围id，可能是岗位id、部门id、市场id，其它类型为空")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Long getTargetRangeId();
+
+    void setTargetRangeId(Long targetRangeId);
+}
