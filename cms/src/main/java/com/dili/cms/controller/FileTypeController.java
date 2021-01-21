9dae4c422b4de30a066c8282103ecd062e6a8763
Tab.Xie <1qaz2wsx>
2021-01-21 13:52:03 +0800

    初始化

--- cms/src/main/java/com/dili/cms/controller/FileTypeController.java
+++ cms/src/main/java/com/dili/cms/controller/FileTypeController.java
@@ -5,17 +5,8 @@
  */
 package com.dili.cms.controller;
 
-import com.dili.cms.domain.FileType;
-import com.dili.cms.service.FileTypeService;
-import com.dili.ss.domain.BaseOutput;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * <pre>
@@ -33,50 +24,5 @@ import java.util.List;
 @Controller
 @RequestMapping("/fileType")
 public class FileTypeController extends BaseController {
-    @Autowired
-    FileTypeService fileTypeService;
 
-
-    @RequestMapping(value = "/index.html", method = RequestMethod.GET)
-    public String index(ModelMap modelMap) {
-        return "fileType/index";
-    }
-
-
-    @RequestMapping(value = "/list.action", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public List<FileType> list(FileType fileType) {
-        return fileTypeService.list(fileType);
-    }
-
-
-    @RequestMapping(value = "/listPage.action", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String listPage(FileType fileType) throws Exception {
-        return fileTypeService.listEasyuiPageByExample(fileType, true).toString();
-    }
-
-
-    @RequestMapping(value = "/insert.action", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public BaseOutput insert(FileType fileType) {
-        fileTypeService.insertSelective(fileType);
-        return BaseOutput.success("新增成功");
-    }
-
-
-    @RequestMapping(value = "/update.action", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public BaseOutput update(FileType fileType) {
-        fileTypeService.updateSelective(fileType);
-        return BaseOutput.success("修改成功");
-    }
-
-
-    @RequestMapping(value = "/delete.action", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public BaseOutput delete(Long id) {
-        fileTypeService.delete(id);
-        return BaseOutput.success("删除成功");
-    }
 }
