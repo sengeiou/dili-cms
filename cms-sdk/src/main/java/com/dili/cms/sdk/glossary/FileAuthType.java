ea939222534751185e29d498c69dc5b1d66e0eb0
Tab.Xie <1qaz2wsx>
2021-01-25 16:49:17 +0800

    文件权限类型枚举

---
+++ cms-sdk/src/main/java/com/dili/cms/sdk/glossary/FileAuthType.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) DiliGroup. All Rights Reserved.
+ * <p>
+ * FileAuthType created on 2021/1/25 11:20 by Tab.Xie
+ */
+package com.dili.cms.sdk.glossary;
+
+/**
+ * <pre>
+ * Description
+ * TODO 文件权限类型枚举
+ *
+ * @author Tab.Xie
+ * @since 1.0
+ *
+ * Change History
+ * Date  Modifier  DESC.
+ * 2021/1/25  Tab.Xie  Initial version.
+ * </pre>
+ */
+public enum FileAuthType {
+    /**
+     * 部门
+     */
+    DEPARTMENT("部门", 10),
+    /**
+     * 市场
+     */
+    MARKET("市场", 20),
+    ;
+
+    private String name;
+    private Integer value;
+
+    private FileAuthType(String name, Integer value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
