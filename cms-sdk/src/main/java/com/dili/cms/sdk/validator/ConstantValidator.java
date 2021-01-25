f81509dea40e01c2311fbed846b8f5bd0863367f
Tab.Xie <1qaz2wsx>
2021-01-25 16:49:05 +0800

    参数通用校验器分组

---
+++ cms-sdk/src/main/java/com/dili/cms/sdk/validator/ConstantValidator.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) DiliGroup. All Rights Reserved.
+ * <p>
+ * ConstantValidator created on 2021/1/25 15:49 by Tab.Xie
+ */
+package com.dili.cms.sdk.validator;
+
+/**
+ * <pre>
+ * Description
+ * TODO 通用校验器
+ *
+ * @author Tab.Xie
+ * @since 1.0
+ *
+ * Change History
+ * Date  Modifier  DESC.
+ * 2021/1/25  Tab.Xie  Initial version.
+ * </pre>
+ */
+public class ConstantValidator {
+    public interface Default {
+    }
+
+    public interface Insert {
+
+    }
+
+    public interface Update {
+
+    }
+
+    public interface Query {
+
+    }
+
+    public interface Page {
+
+    }
+
+    public interface Delete {
+
+    }
+
+    public interface Check {
+
+    }
+}
