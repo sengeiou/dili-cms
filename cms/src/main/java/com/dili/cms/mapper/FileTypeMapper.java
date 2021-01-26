e0a6668c0dcfba0558aef13ccaf64573e60082cf
Tab.Xie <1qaz2wsx>
2021-01-26 17:38:05 +0800

    增加文件类型数量可以减一

--- cms/src/main/java/com/dili/cms/mapper/FileTypeMapper.java
+++ cms/src/main/java/com/dili/cms/mapper/FileTypeMapper.java
@@ -8,6 +8,7 @@ package com.dili.cms.mapper;
 import com.dili.cms.sdk.domain.IFileType;
 import com.dili.ss.base.MyMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Set;
 
@@ -27,11 +28,13 @@ import java.util.Set;
 @Mapper
 public interface FileTypeMapper extends MyMapper<IFileType> {
 
+
     /**
-     * 根据id批量给节点文件的数量+1
+     * 根据id批量给节点文件的数量
      *
      * @param ids
+     * @param num:增加的数量
      * @return
      */
-    int updateNodeCountBatch(Set<Long> ids);
+    int updateNodeCountBatch(@Param("ids") Set<Long> ids, @Param("num") Integer num);
 }
